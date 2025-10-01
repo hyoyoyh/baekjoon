b904d85d5efc432b5b3cdd3b652ab08207392fe3
권효승 <danmuji4049@gmail.com>
2025-10-01 12:14:30 +0900

    1914 하노이의탑 문제

---
+++ 0week/1914.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+def hanoi(n, start, end, mid):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi(n-1,start,mid,end)
+    print(start,end)
+    hanoi(n-1,mid,end,start)
+N = int(input())
+print(2**N-1)
+if N <=20 :
+    hanoi(N,1,3,2)
